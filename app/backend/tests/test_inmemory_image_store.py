f6b51582f15317bde15e2ddb64b26d66e03529d5
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-07-26 14:53:09 +0200

    Changed type of image content in tests.

--- app/backend/tests/test_inmemory_image_store.py
+++ app/backend/tests/test_inmemory_image_store.py
@@ -6,13 +6,13 @@ def test_get_random_image_when_empty_store():
 
 def test_get_random_image_when_one_image():
     store = InMemoryImageStore()
-    image = ("base64image", "a cat")
+    image = (b"base64image", "a cat")
     store.store_image(image[0], image[1])
     assert store.get_random_image()[1] == image[0]
 
 def test_get_image_description():
     store = InMemoryImageStore()
-    store.images = {"id1": ("image_content_1", "cat")}
+    store.images = {"id1": (b"image_content_1", "cat")}
     image_description = store.get_image_description("id1")
     assert image_description == "cat"
 
