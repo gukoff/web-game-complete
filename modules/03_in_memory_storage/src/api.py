70e38d66be96cbe5ac3e7646ea29e2147c0a4df7
Konstantin Gukov <gukkos@gmail.com>
2022-08-28 14:09:49 +0200

    Richer game functionality

--- modules/03_in_memory_storage/src/api.py
+++ modules/03_in_memory_storage/src/api.py
@@ -1,4 +1,6 @@
 from flask import Flask, flash, render_template, request, redirect, session
+from markupsafe import Markup
+
 from .in_memory_storage import InMemoryStorage
 
 APP_VERSION = '0.0.1'
@@ -19,41 +21,48 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/guess', methods=['GET'])
-def guess():
+@app.route('/game', methods=['GET'])
+def game():
     if 'secret_word_id' in session:
-        if database.get_secret_word_by_id(session['secret_word_id']) is not None:
-            return render_template('guess.html')
+        if database.get_word_by_index(session['secret_word_id']) is not None:
+            return render_template('game.html')
 
-    word_id = database.get_random_word_id()
+    word_id = database.get_random_word_index()
     if word_id is None:
         flash("No words uploaded yet! Please upload at least one word to start guessing")
         return redirect("/")
 
     session['secret_word_id'] = word_id
 
-    return render_template('guess.html')
+    return render_template('game.html')
 
 
 @app.route('/make_a_guess', methods=['POST'])
 def make_a_guess():
-    secret_word = database.get_secret_word_by_id(session['secret_word_id'])
+    if 'secret_word_id' not in session:  # this should never happen
+        flash("You can't guess words without starting the game first!")
+        return redirect('/game')
+
+    secret_word = database.get_word_by_index(session['secret_word_id'])
 
     if request.form['guessed_word'] == secret_word:
-        flash("You guessed right! Good job!")
+        flash(Markup("You guessed right! Good job! The secret word was <b>%s</b>" % secret_word))
         del session['secret_word_id']
         return redirect('/')
 
     flash("You didn't guess right! Try again!")
-    return redirect('/guess')
+    return redirect('/game')
 
 
-@app.route('/upload_word', methods=['GET', 'POST'])
+@app.route('/words', methods=['GET'])
+def upload_word_page():
+    return render_template('words.html')
+
+
+@app.route('/upload_word', methods=['POST'])
 def upload_word():
-    if request.method == "GET":
-        return render_template('upload_word.html')
-
     secret_word = request.form['secret']
-    database.add_guess(secret_word)
-    flash("Uploaded the secret " + repr(secret_word))
+    database.add_word(secret_word)
+    flash("Uploaded word " + repr(secret_word))
+    flash("All available words: " + repr(database.get_all_words()))
     return redirect('/')
